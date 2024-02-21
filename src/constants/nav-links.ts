0060c8f3373a6d45ca238b25f787088f6b6fb95e
kmfiguerrez <kmfiguerrez@gmail.com>
2024-02-21 11:28:24 +0800

    Add nav-links.ts constants.

---
+++ src/constants/nav-links.ts
@@ -0,0 +1,18 @@
+export const navigationLinks = [
+    {
+      label: 'HOME',
+      href: '/#home'
+    },
+    {
+      label: 'ABOUT ME',
+      href: '/#about-me'
+    },
+    {
+      label: 'PROJECTS',
+      href: '/#projects'
+    },
+    {
+      label: 'CONTACTS',
+      href: '/#contacts'
+    },
+  ]
